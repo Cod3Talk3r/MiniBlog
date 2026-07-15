eea576eb34db0487b539c7d248c8c02a2f809826
Mohammad R. Asadi <Mohammadr3za.1@gmail.com>
2026-07-15 21:39:00 +0330

    CreateCommentValidator Created.

---
+++ src/MiniBlog.Application/Validators/CreateCommentValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using MiniBlog.Application.DTOs;
+
+namespace MiniBlog.Application.Validators
+{
+    public class CreateCommentValidator : AbstractValidator<CreateCommentDto>
+    {
+        public CreateCommentValidator()
+        {
+            RuleFor(x => x.Text)
+                .NotEmpty().WithMessage("Comment's Text Required")
+                .MaximumLength(500);
+        }
+    }
+}
