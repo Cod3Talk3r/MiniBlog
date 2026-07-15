05bee606ba77608170280d948df4a420f68547b5
Mohammad R. Asadi <Mohammadr3za.1@gmail.com>
2026-07-15 21:27:17 +0330

    PostDto created.

---
+++ src/MiniBlog.Application/DTOs/PostDto.cs
@@ -0,0 +1,20 @@
+﻿namespace MiniBlog.Application.DTOs
+{
+    public record PostDto(
+        int Id,
+        string Title,
+        string Content,
+        DateTime CreatedAt,
+        string AuthorName,
+        int CommentsCount);
+
+    public record CreatePostDto(string Title, string Content);
+
+    public record CommentDto(
+        int Id,
+        string Text,
+        DateTime CreatedAt,
+        string AuthorName);
+
+    public record CreateCommentDto(string Text);
+}
