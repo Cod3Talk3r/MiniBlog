30a3a7e8f96a8eb3dd82f5278d42da977c092f99
Mohammad R. Asadi <Mohammadr3za.1@gmail.com>
2026-07-15 21:35:40 +0330

    CreatePostValidator created.

---
+++ src/MiniBlog.Application/Validators/CreatePostValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using MiniBlog.Application.DTOs;
+
+namespace MiniBlog.Application.Validators
+{
+    public class CreatePostValidator : AbstractValidator<CreatePostDto>
+    {
+        public CreatePostValidator()
+        {
+            RuleFor(x => x.Title)
+                .NotEmpty().WithMessage("Title Required")
+                .MaximumLength(150);
+
+            RuleFor(x => x.Content)
+                .NotEmpty().WithMessage("Content Required")
+                .MinimumLength(10);
+        }
+    }
+}
