0c1d3f1bc3360b1c5a06a5173861a47314399337
Mohammad R. Asadi <Mohammadr3za.1@gmail.com>
2026-07-15 23:00:06 +0330

    adding Web/Controllers/PostsController.cs

---
+++ src/MiniBlog.Web/Controllers/AccountController.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MiniBlog.Application.DTOs;
+using MiniBlog.Application.Interfaces;
+using MiniBlog.Domain.Entities;
+
+namespace MiniBlog.Web.Controllers
+{
+    [ApiController]
+    [Route("api/account")]
+    public class AccountController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly ITokenService _tokenService;
+
+        public AccountController(
+            UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager,
+            ITokenService tokenService)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _tokenService = tokenService;
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<AuthResultDto>> Register(RegisterDto dto, CancellationToken cancellationToken)
+        {
+            var user = new ApplicationUser { UserName = dto.UserName, Email = dto.Email };
+            var result = await _userManager.CreateAsync(user, dto.Password);
+
+            if (!result.Succeeded)
+            {
+                var errors = string.Join("; ", result.Errors.Select(e => e.Description));
+                return BadRequest(new AuthResultDto(false, null, errors));
+            }
+
+            var token = _tokenService.GenerateToken(user);
+            SetTokenCookie(token);
+
+            return Ok(new AuthResultDto(true, token, null));
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<AuthResultDto>> Login(LoginDto dto, CancellationToken cancellationToken)
+        {
+            var user = await _userManager.FindByEmailAsync(dto.Email);
+            if (user is null)
+                return Unauthorized(new AuthResultDto(false, null, "ایمیل یا رمز عبور اشتباه است"));
+
+            var checkResult = await _signInManager.CheckPasswordSignInAsync(user, dto.Password, lockoutOnFailure: false);
+            if (!checkResult.Succeeded)
+                return Unauthorized(new AuthResultDto(false, null, "ایمیل یا رمز عبور اشتباه است"));
+
+            var token = _tokenService.GenerateToken(user);
+            SetTokenCookie(token);
+
+            return Ok(new AuthResultDto(true, token, null));
+        }
+
+        [HttpPost("logout")]
+        public IActionResult Logout()
+        {
+            Response.Cookies.Delete("access_token");
+            return Ok();
+        }
+
+        private void SetTokenCookie(string token)
+        {
+            Response.Cookies.Append("access_token", token, new CookieOptions
+            {
+                HttpOnly = true,
+                Secure = true,
+                SameSite = SameSiteMode.Strict,
+                Expires = DateTimeOffset.UtcNow.AddHours(1)
+            });
+        }
+    }
+}
