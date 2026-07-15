df72372f57f902d3cb5daf5d28f6231b8b9aa972
Mohammad R. Asadi <Mohammadr3za.1@gmail.com>
2026-07-15 22:58:55 +0330

    Adding Application/DTOs/AuthDtos.cs

---
+++ src/MiniBlog.Application/DTOs/AuthDtos.cs
@@ -0,0 +1,6 @@
+﻿namespace MiniBlog.Application.DTOs
+{
+    public record RegisterDto(string UserName, string Email, string Password);
+    public record LoginDto(string Email, string Password);
+    public record AuthResultDto(bool Success, string? Token, string? Error);
+}
