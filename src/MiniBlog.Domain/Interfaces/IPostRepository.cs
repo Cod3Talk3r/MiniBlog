c9755cbc9b95f5005f571f29efcebf4550b5be2d
Mohammad R. Asadi <Mohammadr3za.1@gmail.com>
2026-07-15 21:02:46 +0330

    IPostRepository Created.

---
+++ src/MiniBlog.Domain/Interfaces/IPostRepository.cs
@@ -0,0 +1,12 @@
+﻿using MiniBlog.Domain.Entities;
+
+namespace MiniBlog.Domain.Interfaces
+{
+    public interface IPostRepository
+    {
+        IQueryable<Post> GetAllQueryable();
+        Task<Post?> GetByIdAsync(int id, CancellationToken cancellationToken = default);
+        Task AddAsync(Post post, CancellationToken cancellationToken = default);
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+    }
+}
