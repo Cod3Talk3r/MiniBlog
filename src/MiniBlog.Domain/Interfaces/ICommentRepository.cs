aa34fad35f406321ed73865ac6f97cbc595227a6
Mohammad R. Asadi <Mohammadr3za.1@gmail.com>
2026-07-15 22:16:13 +0330

    adding ICommentRepository Interface to Domain Interface.

---
+++ src/MiniBlog.Domain/Interfaces/ICommentRepository.cs
@@ -0,0 +1,11 @@
+﻿using MiniBlog.Domain.Entities;
+
+namespace MiniBlog.Domain.Interfaces
+{
+    public interface ICommentRepository
+    {
+        Task<List<Comment>> GetByPostIdAsync(int postId, CancellationToken cancellationToken = default);
+        Task AddAsync(Comment comment, CancellationToken cancellationToken = default);
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+    }
+}
