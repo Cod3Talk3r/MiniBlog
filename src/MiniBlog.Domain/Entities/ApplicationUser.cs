5b51529518fba5ff90f597876e46046357eb69b1
Mohammad R. Asadi <Mohammadr3za.1@gmail.com>
2026-07-15 20:48:18 +0330

    ApplicationUser Entitie Created.

---
+++ src/MiniBlog.Domain/Entities/ApplicationUser.cs
@@ -0,0 +1,8 @@
+﻿namespace MiniBlog.Domain.Entities
+{
+    public class ApplicationUser
+    {
+        public ICollection<Post> Posts { get; set; } = new List<Post>();
+        public ICollection<Comment> Comments { get; set; } = new List<Comment>();
+    }
+}
