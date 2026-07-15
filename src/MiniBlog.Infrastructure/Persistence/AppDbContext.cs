53a49916e5d8502a4191c1b846b546be841b5bf8
Mohammad R. Asadi <Mohammadr3za.1@gmail.com>
2026-07-15 22:02:27 +0330

    creating DbContext.

---
+++ src/MiniBlog.Infrastructure/Persistence/AppDbContext.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using MiniBlog.Domain.Entities;
+
+namespace MiniBlog.Infrastructure.Persistence
+{
+    public class AppDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Post> Posts => Set<Post>();
+        public DbSet<Comment> Comments => Set<Comment>();
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<Post>(entity =>
+            {
+                entity.Property(p => p.Title).HasMaxLength(150).IsRequired();
+                entity.HasOne(p => p.Author)
+                      .WithMany(u => u.Posts)
+                      .HasForeignKey(p => p.AuthorId)
+                      .OnDelete(DeleteBehavior.Restrict);
+            });
+
+            builder.Entity<Comment>(entity =>
+            {
+                entity.Property(c => c.Text).HasMaxLength(500).IsRequired();
+                entity.HasOne(c => c.Post)
+                      .WithMany(p => p.Comments)
+                      .HasForeignKey(c => c.PostId)
+                      .OnDelete(DeleteBehavior.Cascade);
+
+                entity.HasOne(c => c.Author)
+                      .WithMany(u => u.Comments)
+                      .HasForeignKey(c => c.AuthorId)
+                      .OnDelete(DeleteBehavior.Restrict);
+            });
+
+            // for query performance on CreatedAt
+            builder.Entity<Post>().HasIndex(p => p.CreatedAt);
+        }
+    }
+}
