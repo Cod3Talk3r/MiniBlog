65883ff65ce361433d0be806cf2edbd692219684
Mohammad R. Asadi <Mohammadr3za.1@gmail.com>
2026-07-15 23:07:08 +0330

    Adding Web/Pages/Account/Register.cshtml.cs for UI

---
+++ src/MiniBlog.Web/Pages/Account/Register.cshtml.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using MiniBlog.Application.Interfaces;
+using MiniBlog.Domain.Entities;
+
+namespace MiniBlog.Web.Pages.Account
+{
+    public class RegisterModel : PageModel
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly ITokenService _tokenService;
+
+        public RegisterModel(UserManager<ApplicationUser> userManager, ITokenService tokenService)
+        {
+            _userManager = userManager;
+            _tokenService = tokenService;
+        }
+
+        [BindProperty]
+        public InputModel Input { get; set; } = new();
+
+        public string? ErrorMessage { get; set; }
+
+        public class InputModel
+        {
+            public string UserName { get; set; } = default!;
+            public string Email { get; set; } = default!;
+            public string Password { get; set; } = default!;
+        }
+
+        public void OnGet() { }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid) return Page();
+
+            var user = new ApplicationUser { UserName = Input.UserName, Email = Input.Email };
+            var result = await _userManager.CreateAsync(user, Input.Password);
+
+            if (!result.Succeeded)
+            {
+                ErrorMessage = string.Join("; ", result.Errors.Select(e => e.Description));
+                return Page();
+            }
+
+            var token = _tokenService.GenerateToken(user);
+            Response.Cookies.Append("access_token", token, new CookieOptions
+            {
+                HttpOnly = true,
+                Secure = true,
+                SameSite = SameSiteMode.Strict,
+                Expires = DateTimeOffset.UtcNow.AddHours(1)
+            });
+
+            return RedirectToPage("/Posts/Index");
+        }
+    }
+}
