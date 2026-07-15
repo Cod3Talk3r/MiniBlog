7726891ec51cca2c7e5f6335dce9fa76dabc293e
Mohammad R. Asadi <Mohammadr3za.1@gmail.com>
2026-07-15 20:39:22 +0330

    post entite completed.

--- src/MiniBlog.Domain/Entities/Post.cs
+++ src/MiniBlog.Domain/Entities/Post.cs
@@ -4,5 +4,11 @@
     {
         public int Id { get; set; }
         public string Title { get; set; } = default!;
+        public string Content { get; set; } = default!;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public string AuthorId { get; set; } = default!;
+        public ApplicationUser Author { get; set; } = default!;
+
+        public ICollection<Comment> Comments { get; set; } = new List<Comment>();
     }
 }
