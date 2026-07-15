68872a874ac62d21205fea3b01371eee1ffb1817
Mohammad R. Asadi <Mohammadr3za.1@gmail.com>
2026-07-15 22:42:59 +0330

    Adding MiniBlog.Web/Filters/LoggingActionFilter.cs for Logging.

---
+++ src/MiniBlog.Web/Filters/LoggingActionFilter.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace MiniBlog.Web.Filters
+{
+    public class LoggingActionFilter : IActionFilter
+    {
+        private readonly ILogger<LoggingActionFilter> _logger;
+
+        public LoggingActionFilter(ILogger<LoggingActionFilter> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            _logger.LogInformation("Executing {Action} with args {@Args}",
+                context.ActionDescriptor.DisplayName, context.ActionArguments);
+        }
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            _logger.LogInformation("Executed {Action}, Result: {ResultType}",
+                context.ActionDescriptor.DisplayName, context.Result?.GetType().Name);
+        }
+    }
+}
