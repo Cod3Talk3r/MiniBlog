8e1a804886bb2246eb06417250ed81eb044b1fdb
Mohammad R. Asadi <Mohammadr3za.1@gmail.com>
2026-07-15 21:42:04 +0330

    IPostService created and wrote.

---
+++ src/MiniBlog.Application/Interfaces/IPostService.cs
@@ -0,0 +1,14 @@
+﻿using MiniBlog.Application.DTOs;
+
+namespace MiniBlog.Application.Interfaces
+{
+    public interface IPostService
+    {
+        Task<(IEnumerable<PostDto> Items, int TotalCount)> GetPagedAsync(
+        int page, int pageSize, string? search, CancellationToken cancellationToken = default);
+
+        Task<PostDto?> GetByIdAsync(int id, CancellationToken cancellationToken = default);
+
+        Task<PostDto> CreateAsync(CreatePostDto dto, string authorId, CancellationToken cancellationToken = default);
+    }
+}
