bca744f0212e17e2e6e97d93c4de2a27faa9038e
Mohammad R. Asadi <Mohammadr3za.1@gmail.com>
2026-07-15 21:43:52 +0330

    ICommentService created.

---
+++ src/MiniBlog.Application/Interfaces/ICommentService.cs
@@ -0,0 +1,11 @@
+﻿using MiniBlog.Application.DTOs;
+
+namespace MiniBlog.Application.Interfaces
+{
+    public interface ICommentService
+    {
+        Task<IEnumerable<CommentDto>> GetByPostIdAsync(int postId, CancellationToken cancellationToken = default);
+
+        Task<CommentDto> AddAsync(int postId, CreateCommentDto dto, string authorId, CancellationToken cancellationToken = default);
+    }
+}
