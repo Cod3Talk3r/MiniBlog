48c71061a4269fcfbdc07428f184d18901fc8c93
Mohammad R. Asadi <Mohammadr3za.1@gmail.com>
2026-07-15 20:47:46 +0330

    Comment Entitie created and completed.

---
+++ src/MiniBlog.Domain/Entities/Comment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MiniBlog.Domain.Entities
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public string Text { get; set; } = default!;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public int PostId { get; set; }
+        public Post Post { get; set; } = default!;
+
+        public string AuthorId { get; set; } = default!;
+        public ApplicationUser Author { get; set; } = default!;
+    }
+}
