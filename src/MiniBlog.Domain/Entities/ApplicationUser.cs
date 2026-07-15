24967c63fa3a7c6cec05bb3d753b98e9b6011655
Mohammad R. Asadi <Mohammadr3za.1@gmail.com>
2026-07-15 22:31:33 +0330

    compeleting Domain/Entities/ApplicationUser.cs file

--- src/MiniBlog.Domain/Entities/ApplicationUser.cs
+++ src/MiniBlog.Domain/Entities/ApplicationUser.cs
@@ -1,6 +1,8 @@
-﻿namespace MiniBlog.Domain.Entities
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace MiniBlog.Domain.Entities
 {
-    public class ApplicationUser
+    public class ApplicationUser : IdentityUser
     {
         public ICollection<Post> Posts { get; set; } = new List<Post>();
         public ICollection<Comment> Comments { get; set; } = new List<Comment>();
