afaedbce5795549b6de0b2d9c6cf4c9c220ba84b
Mohammad R. Asadi <Mohammadr3za.1@gmail.com>
2026-07-15 23:38:19 +0330

    Logout Page Created.

---
+++ src/MiniBlog.Web/Pages/Account/Logout.cshtml.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MiniBlog.Web.Pages.Account
+{
+    public class LogoutModel : PageModel
+    {
+        public IActionResult OnPost()
+        {
+            Response.Cookies.Delete("access_token");
+            return RedirectToPage("/Posts/Index");
+        }
+    }
+}
