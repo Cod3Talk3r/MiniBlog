6f57ab96322a92f95b231d4faf1b31984f3c8141
Mohammad R. Asadi <Mohammadr3za.1@gmail.com>
2026-07-15 22:57:13 +0330

    Adding Application/Mapping/MappingProfile.cs

---
+++ src/MiniBlog.Application/Mapping/MappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using MiniBlog.Application.DTOs;
+using MiniBlog.Domain.Entities;
+
+namespace MiniBlog.Application.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Post, PostDto>()
+                .ForCtorParam("AuthorName", opt => opt.MapFrom(src => src.Author.UserName))
+                .ForCtorParam("CommentsCount", opt => opt.MapFrom(src => src.Comments.Count));
+
+            CreateMap<Comment, CommentDto>()
+                .ForCtorParam("AuthorName", opt => opt.MapFrom(src => src.Author.UserName));
+        }
+    }
+}
